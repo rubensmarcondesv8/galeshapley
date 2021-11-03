e14994f4f2e6e5dda847d0c18d5c5797a8c4764c
Rubens Marcondes <rubensmarcondesv8@gmail.com>
2021-11-03 01:54:54 +0000

    alocacao de memoria

--- main.cpp
+++ main.cpp
@@ -7,30 +7,46 @@ int main(){
   unsigned **hPref;
   unsigned **sPref;
 
-  unsigned *M;
+  //array de ponteiros que guarda o próximo estudante que receberá proposta de cada unmatchHospital
+
+  unsigned *H;
   unsigned *S;
 
+  std::ifstream fin("teste.txt");
   hPref = (unsigned**)malloc(n * sizeof(unsigned*));
   sPref = (unsigned**)malloc(n * sizeof(unsigned*));
-
-  M = (unsigned*)malloc(n * sizeof(unsigned));
+  for(unsigned k = 0; k < n; k++){
+    hPref[k] = (unsigned*)malloc(n * sizeof(unsigned));
+    sPref[k] = (unsigned*)malloc(n * sizeof(unsigned));
+  }
+  
+  //array invertido das preferências
+  H = (unsigned*)malloc(n * sizeof(unsigned));
   S = (unsigned*)malloc(n * sizeof(unsigned));
 
-  for(int i = 1; i <= n){
-    M[i - 1] = i;
-    S[i - 1] = i;
+  for(unsigned i = 0; i < n; i++){
+    H[i] = 0;
+    S[i] = 0;
   }
 
-  std::ifstream fin("teste.txt");
+  std::queue<unsigned> unmatchHospital;
+  for(unsigned i = 1; i <= n; i++){
+    unmatchHospital.push(i);
+  }
 
-  for(int i = 0; i < n; i++){
+  while(!unmatchHospital.empty()){
+    //casamento estavel
+  }
+
+  for(unsigned i = 0; i < n; i++){
 	  free(hPref[i]);
     free(sPref[i]);
   }
 
 	free(hPref);
   free(sPref);
-  free(M);
+  free(H);
   free(S);
 
+  return 0;
 }
