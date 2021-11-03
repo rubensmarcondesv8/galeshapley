5dc79396febcf9d2f03ba7aa988987ebfad42dd6
Rubens Marcondes <rubensmarcondesv8@gmail.com>
2021-11-03 01:14:40 +0000

    comit inicial

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream> 
+
+int main(){
+  unsigned n = 5
+  unsigned **hPref;
+  unsigned **sPref;
+
+  unsigned *M;
+  unsigned *S;
+
+  hPref = (unsigned**)malloc(n * sizeof(unsigned*));
+  sPref = (unsigned**)malloc(n * sizeof(unsigned*));
+
+  M = (unsigned*)malloc(n * sizeof(unsigned));
+  S = (unsigned*)malloc(n * sizeof(unsigned));
+
+  for(int i = 1; i <= n){
+    M[i - 1] = i;
+    S[i - 1] = i;
+  }
+
+  std::ifstream fin("teste.txt");
+
+  for(int i = 0; i < n; i++){
+	  free(hPref[i]);
+     free(sPref[i]);
+  }
+
+	free(hPref);
+  free(sPref);
+
+}
